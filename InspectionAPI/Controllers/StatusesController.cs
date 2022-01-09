b73cb311d192f6a30168a9eac8276c332011dde5
Chris <eng.christhuku@gmail.com>
2022-01-09 14:02:51 +0300

    feat generate controllers

---
+++ InspectionAPI/Controllers/StatusesController.cs
@@ -0,0 +1,109 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using InspectionAPI.Data;
+using InspectionAPI.Models;
+
+namespace InspectionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusesController : ControllerBase
+    {
+        private readonly InspectionContext _context;
+
+        public StatusesController(InspectionContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Statuses
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Status>>> GetStatuss()
+        {
+            return await _context.Statuss.ToListAsync();
+        }
+
+        // GET: api/Statuses/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Status>> GetStatus(Guid id)
+        {
+            var status = await _context.Statuss.FindAsync(id);
+
+            if (status == null)
+            {
+                return NotFound();
+            }
+
+            return status;
+        }
+
+        // PUT: api/Statuses/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStatus(Guid id, Status status)
+        {
+            if (id != status.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(status).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StatusExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Statuses
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Status>> PostStatus(Status status)
+        {
+            _context.Statuss.Add(status);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetStatus", new { id = status.Id }, status);
+        }
+
+        // DELETE: api/Statuses/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStatus(Guid id)
+        {
+            var status = await _context.Statuss.FindAsync(id);
+            if (status == null)
+            {
+                return NotFound();
+            }
+
+            _context.Statuss.Remove(status);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool StatusExists(Guid id)
+        {
+            return _context.Statuss.Any(e => e.Id == id);
+        }
+    }
+}

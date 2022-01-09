76ac866ff7863cd043d0f78dd9bee662435e5cd7
Chris <eng.christhuku@gmail.com>
2022-01-09 13:49:04 +0300

    enable cors

--- InspectionAPI/Program.cs
+++ InspectionAPI/Program.cs
@@ -1,6 +1,7 @@
 using InspectionAPI.Data;
 using Microsoft.EntityFrameworkCore;
 
+var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,12 +10,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+//Add DBContext
 builder.Services.AddDbContext<InspectionContext>(options =>
 {
   options.UseSqlServer(builder.Configuration.GetConnectionString("InspectionDbConnection"));
 });
-
+//Enable Cors
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy(name: myAllowSpecificOrigins, builder =>
+   {
+     builder.WithOrigins("http://localhost:4200")
+     .AllowAnyHeader()
+     .AllowAnyMethod();
+   });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,6 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+//Use Cors
+app.UseCors(myAllowSpecificOrigins);
 
 app.UseAuthorization();
 

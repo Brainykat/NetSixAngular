1f34fa1345c4706a241813bfa8a8dcc07fe004e5
Chris <eng.christhuku@gmail.com>
2022-01-09 22:49:16 +0300

    feat add show inspection list

--- FrontEnd/angular13-inspaction-app/src/app/inspection/show-inspection/show-inspection.component.ts
+++ FrontEnd/angular13-inspaction-app/src/app/inspection/show-inspection/show-inspection.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, observable } from 'rxjs';
+import { InspectionApiService } from 'src/app/inspection-api.service';
 
 @Component({
   selector: 'app-show-inspection',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowInspectionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:InspectionApiService) { }
+
+  inspectionList$!:Observable<any[]>;
+  inspectionTypeList$!:Observable<any[]>;
+  inspectionTypeList:any=[];
+
+  //Map to display data assosiated with Foreign keys
+  inspectionTypesMap:Map<string,string> = new Map();
 
   ngOnInit(): void {
+    this.inspectionList$ = this.service.getInspectionList();
   }
 
 }
